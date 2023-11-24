7627802067550aac0a799a08c2b986ea33124cd2
Yeon73 <yunalove7373@gmail.com>
2023-11-25 08:30:04 +0900

    Fix: API 요청 오류 수정
    api 수정 + 이름 -> 닉네임으로 변경

--- aegis/src/main/java/com/seoultech/tools/aegis/controller/HomeController.java
+++ aegis/src/main/java/com/seoultech/tools/aegis/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     // 기본페이지 요청 메서드
-    @GetMapping("/aegis")
+    @GetMapping("/")
     public String index() {
         return "index";  // => templates 폴더의 index.html을 찾아감
     }
