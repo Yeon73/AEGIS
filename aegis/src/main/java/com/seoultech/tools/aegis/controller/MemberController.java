ec89be962f34ebb10425b239f832ee8418b79f71
Kim Byeongyeon <yunalove7373@gmail.com>
2023-11-29 22:11:55 +0900

    Fix: 리다이렉트 삭제

--- aegis/src/main/java/com/seoultech/tools/aegis/controller/MemberController.java
+++ aegis/src/main/java/com/seoultech/tools/aegis/controller/MemberController.java
@@ -3,28 +3,34 @@ package com.seoultech.tools.aegis.controller;
 import com.seoultech.tools.aegis.dto.MemberDTO;
 import com.seoultech.tools.aegis.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 
-@Controller
-@RequiredArgsConstructor
+@RestController
+@RequestMapping("/member")
 public class MemberController {
-    // 생성자 주입
+
     private final MemberService memberService;
 
-    @GetMapping("/member/game")
+    // 생성자 주입
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/game")
     public String game() {
         return "game";
     }
 
-    // 회원가입 페이지 출력 요청
-    @GetMapping("/member/save")
+    @GetMapping("/save")
     public String saveForm() {
         return "save";
     }
 
-    @PostMapping("/member/save")
+    @PostMapping("/save")
     public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
@@ -32,10 +38,12 @@ public class MemberController {
         return "login";
     }
 
-    @GetMapping("/member/login")
-    public String loginForm() { return "login"; }
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
 
-    @PostMapping("/member/login")
+    @PostMapping("/login")
     public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);
         if (loginResult != null) {
@@ -49,44 +57,46 @@ public class MemberController {
         }
     }
 
-    @GetMapping("/member/logout")
+    @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "index";
     }
 
-    @PostMapping("/member/email-check")
+    @PostMapping("/email-check")
     public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
         System.out.println("memberEmail = " + memberEmail);
         String checkResult = memberService.emailCheck(memberEmail);
         return checkResult;
     }
 
-    @GetMapping("/member/progress")
+    @GetMapping("/progress")
     public String progressForm() {
         return "progress";
     }
 
-    @GetMapping("/member/achievement")
+    @GetMapping("/achievement")
     public String achievementForm() {
         return "achievement";
     }
 
-    @GetMapping("/member/profile")
+    @GetMapping("/profile")
     public String profileForm() {
         return "profile";
     }
 
-    @PostMapping("/member/start-game")
-    public String startGame(HttpSession session) {
+    @PostMapping("/isLogin")
+    public ResponseEntity<Boolean> checkLoginStatus(HttpSession session) {
         Boolean isLoggedIn = (Boolean) session.getAttribute("isLogin");
 
         if (isLoggedIn != null && isLoggedIn) {
-            // 로그인 상태이면 game 페이지로 이동
-            return "redirect:/member/game";
+            // 로그인 상태이면 true 반환
+            return ResponseEntity.ok(true);
         } else {
-            // 비로그인 상태이면 login 페이지로 리다이렉트
-            return "redirect:/member/login";
+            // 비로그인 상태이면 false 반환
+            return ResponseEntity.ok(false);
         }
     }
+
 }
+
